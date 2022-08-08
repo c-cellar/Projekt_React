9475e5f36570130752caf3cb39e5cd83197ea4f4
c-cellar <christian.l.keller@outlook.com>
2022-08-08 21:53:41 +0200

    add function to fetch movies with ids

--- js/src/components/Movie.jsx
+++ js/src/components/Movie.jsx
@@ -1,3 +1,39 @@
+import movies from '../moviesToWatch';
+import { randomNumber } from '../helpers';
+import { useState } from 'react';
+import RandomMovie from './RandomMovie';
+
+const OMDd = 'http://www.omdbapi.com/?apikey=1d847164';
+
 export default function Movie() {
-  return <div>Movie</div>;
+  const [movieDetails, setMovieDetails] = useState([]);
+
+  async function getRandomMovieFromArray() {
+    // get a random id from array movies;
+    const movieId = movies[randomNumber(movies.length)];
+
+    try {
+      const response = await fetch(`${OMDd}&i=${movieId}`);
+
+      if (!response.ok) {
+        throw new Error('Could not load movie data');
+      }
+
+      const jsonData = await response.json();
+
+      setMovieDetails(jsonData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <section className="section--movie">
+        <h2>Movie</h2>
+        {movieDetails && <RandomMovie movieDetails={movieDetails} />}
+        <button onClick={getRandomMovieFromArray}>get a film</button>
+      </section>
+    </div>
+  );
 }
