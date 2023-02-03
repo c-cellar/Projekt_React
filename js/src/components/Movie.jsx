44be0662100eddb9036face769688c404894b877
c-cellar <christian.l.keller@outlook.com>
2023-02-03 13:47:23 +0100

    -implemented searchParams hook

--- js/src/components/Movie.jsx
+++ js/src/components/Movie.jsx
@@ -1,31 +1,47 @@
 import movies from '../moviesToWatch';
 import { randomNumber } from '../helpers';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import RandomMovie from './RandomMovie';
 
 const OMDd = 'https://www.omdbapi.com/?apikey=1d847164';
 
-export default function Movie() {
+export default function Movie({ searchParams, setSearchParams }) {
+  const [movieId, setMovieId] = useState();
   const [movieDetails, setMovieDetails] = useState([]);
 
-  async function getRandomMovieFromArray() {
-    // get a random id from array movies;
-    const movieId = movies[randomNumber(movies.length)];
-
-    try {
-      const response = await fetch(`${OMDd}&i=${movieId}`);
-
-      if (!response.ok) {
-        throw new Error('Could not load movie data');
-      }
-
-      const jsonData = await response.json();
-
-      setMovieDetails(jsonData);
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    if (searchParams.get('fID') !== null) {
+      setMovieId(searchParams.get('fID'));
     }
+  }, []);
+
+  function getRandomMovieFromArray() {
+    const movieId = movies[randomNumber(movies.length)];
+    setMovieId(movieId);
   }
+
+  useEffect(() => {
+    async function fetchMovieDetailsFromAPI() {
+      try {
+        const response = await fetch(`${OMDd}&i=${movieId}`);
+
+        if (!response.ok) {
+          throw new Error('Could not load movie data');
+        }
+
+        const jsonData = await response.json();
+
+        setMovieDetails(jsonData);
+
+        searchParams.set('fID', movieId);
+        setSearchParams(searchParams);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchMovieDetailsFromAPI();
+  }, [movieId]);
 
   return (
     <div>
@@ -33,7 +49,7 @@ export default function Movie() {
         <h2>
           <span className="h2--movie">Movie</span>
         </h2>
-        <RandomMovie movieDetails={movieDetails} />
+        {movieDetails.Title && <RandomMovie movieDetails={movieDetails} />}
         <div className="button--container">
           <button onClick={getRandomMovieFromArray}>
             {movieDetails == 0 ? 'get a film' : 'roll again'}
