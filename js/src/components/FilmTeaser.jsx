93079747da6ea04b5d0b30f2a14177956bdf0d2c
c-cellar <christian.l.keller@outlook.com>
2023-03-29 17:52:59 +0200

    -added h3 -added shorthand fragment around component

--- js/src/components/FilmTeaser.jsx
+++ js/src/components/FilmTeaser.jsx
@@ -11,59 +11,62 @@ export default function FilmTeaser({
   watchList,
 }) {
   return (
-    <div className="filmTeaser-wrapper">
-      {resultsUserInput.map(({ Title, Poster, Year, imdbID, Type }) => (
-        <article className="article--teaser" key={imdbID}>
-          {Poster !== 'N/A' ? (
-            <img
-              className="image--teaser"
-              src={Poster}
-              alt={`There is no film poster for ${Title}`}
-            />
-          ) : (
-            <p>There is no film poster for: {Title}</p>
-          )}
-          <h4>
-            {Title} ({Year && <time dateTime={Year}>{Year}</time>})
-          </h4>
-          <div className="button-wrapper">
-            {watchList.some((film) => film.id === imdbID) ? (
-              <button
-                aria-label={`remove ${Title} from watch list`}
-                className={'secondary'}
-                onClick={() =>
-                  watchListDispatch({
-                    id: imdbID,
-                    title: Title,
-                    type: Type,
-                    poster: Poster,
-                    action: 'remove',
-                  })
-                }
-              >
-                remove
-              </button>
+    <>
+      <h3>results</h3>
+      <div className="filmTeaser-wrapper">
+        {resultsUserInput.map(({ Title, Poster, Year, imdbID, Type }) => (
+          <article className="article--teaser" key={imdbID}>
+            {Poster !== 'N/A' ? (
+              <img
+                className="image--teaser"
+                src={Poster}
+                alt={`There is no film poster for ${Title}`}
+              />
             ) : (
-              <button
-                aria-label={`add ${Title} to watch list`}
-                className={'primary'}
-                onClick={() =>
-                  watchListDispatch({
-                    id: imdbID,
-                    title: Title,
-                    type: Type,
-                    poster: Poster,
-                    action: 'add',
-                  })
-                }
-              >
-                add to watchlist
-              </button>
+              <p>There is no film poster for: {Title}</p>
             )}
-          </div>
-        </article>
-      ))}
-      <button className="button--toTop" onClick={scrollToTop}></button>
-    </div>
+            <h4>
+              {Title} ({Year && <time dateTime={Year}>{Year}</time>})
+            </h4>
+            <div className="button-wrapper">
+              {watchList.some((film) => film.id === imdbID) ? (
+                <button
+                  aria-label={`remove ${Title} from watch list`}
+                  className="secondary"
+                  onClick={() =>
+                    watchListDispatch({
+                      id: imdbID,
+                      title: Title,
+                      type: Type,
+                      poster: Poster,
+                      action: 'remove',
+                    })
+                  }
+                >
+                  remove
+                </button>
+              ) : (
+                <button
+                  aria-label={`add ${Title} to watch list`}
+                  className="primary"
+                  onClick={() =>
+                    watchListDispatch({
+                      id: imdbID,
+                      title: Title,
+                      type: Type,
+                      poster: Poster,
+                      action: 'add',
+                    })
+                  }
+                >
+                  add to watchlist
+                </button>
+              )}
+            </div>
+          </article>
+        ))}
+        <button className="button--toTop" onClick={scrollToTop}></button>
+      </div>
+    </>
   );
 }
